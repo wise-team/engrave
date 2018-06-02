8d97510fdc5ebe73ce55a499462bb0b0bc844386
Bartłomiej Górnicki <brutusgsm@gmail.com>
2018-06-02 02:48:12 +0200

    Add database url to default blog config

--- modules/nodeapps.js
+++ modules/nodeapps.js
@@ -24,6 +24,7 @@ module.exports.createAndRun = (domain, port, steem_username) => {
                     env: {
                         PORT: port,
                         EDITORIAL_STEEM_USERNAME: steem_username,
+                        DATABASE_URL: config.database_url,
                         NODE_ENV: "development"
                     },
                 }]
