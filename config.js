9f565acf1edd89b9ae29421515917d20886cadd9
Bartłomiej Górnicki <brutusgsm@gmail.com>
2018-06-05 02:50:48 +0200

    Engrave steemconnect app. Demo YT video deleted

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let config = {
     port: process.env.PORT || "8080",
     session_secret: process.env.SESSION_SECRET || 'PcxgE3gU3uyhed9LdfIRMHTpF9UXI7glEO1AYlKO',
     database_url: process.env.DATABASE_URL || "mongodb://user:password@server.com",
-    steemconnect_id: process.env.STEEMCONNECT_ID || 'glodniwiedzy.app',
+    steemconnect_id: process.env.STEEMCONNECT_ID || 'engrave.app',
     steemconnect_redirect_uri: process.env.STEEMCONNECT_REDIRECT_URI || 'https://localhost/authorize/',
     beneficiary: process.env.BENEFICIARY || "tiploo",
     domain: process.env.DOMAIN || 'engrave.site'
