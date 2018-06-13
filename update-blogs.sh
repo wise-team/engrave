fcdea38ced7c628347830ff8b003c2e24d0318bc
Bartłomiej Górnicki <brutusgsm@gmail.com>
2018-06-13 21:56:53 +0200

    Script for blogs updating

--- update-blogs.sh
+++ update-blogs.sh
@@ -1,4 +1,7 @@
-#!/bin/bash  
+#!/bin/bash 
+
+# Copy all necessary files from blog template to every running blog on platform
+
 for i in $(find pm2_blogs/ -maxdepth 1 ! -path pm2_blogs/ -type d);
 do
     BLOG=$(echo $i | sed 's#pm2_blogs/##'); # pm2 process is equal to directory name but without trailing dot and slash
