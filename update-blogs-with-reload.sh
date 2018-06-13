0ccfd3a2e3cfc92d83bff863f0bab722bc7d7af0
Bartłomiej Górnicki <brutusgsm@gmail.com>
2018-06-13 21:59:08 +0200

    Blog updatings script with and without reloading

--- update-blogs-with-reload.sh
+++ update-blogs-with-reload.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
 # Copy all necessary files from blog template to every running blog on platform
-# It does not reload PM2 apps. Can be used when only visual fixes are made
 
 for i in $(find pm2_blogs/ -maxdepth 1 ! -path pm2_blogs/ -type d);
 do
     BLOG=$(echo $i | sed 's#pm2_blogs/##'); # pm2 process is equal to directory name but without trailing dot and slash
     echo $BLOG;
     sudo cp -r blog_app_template/* $i/; # copy all files
+    sudo pm2 reload $BLOG # restart blog instance
 done
