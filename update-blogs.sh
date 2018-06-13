320151cf0400bfadd2371a88df8c09ce70f19ff6
Bartłomiej Górnicki <brutusgsm@gmail.com>
2018-06-13 14:39:28 +0200

    Update blogs script

---
+++ update-blogs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash  
+for i in $(find pm2_blogs/ -maxdepth 1 ! -path pm2_blogs/ -type d);
+do
+    BLOG=$(echo $i | sed 's#pm2_blogs/##'); # pm2 process is equal to directory name but without trailing dot and slash
+    echo $BLOG;
+    sudo cp -r blog_app_template/* $i/; # copy all files
+    sudo pm2 reload $BLOG # restart blog instance
+done
