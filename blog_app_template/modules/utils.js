9811774f7de051db3a7af677683e38a2831aaefb
Bartłomiej Górnicki <brutusgsm@gmail.com>
2018-08-07 23:32:24 +0200

    Removing blogs information fixed

--- blog_app_template/modules/utils.js
+++ blog_app_template/modules/utils.js
@@ -427,7 +427,7 @@ module.exports.removeWebsiteAdvertsElements = (body) => {
     let a = body.replace("***********\n\n### " + cfg.get_config().info_website_info_text, "");
     let b = a.replace(/(\*\*\*\*\*\*\*\*\*\*\*\n\nArtykuł autorstwa: @)(?:.*)(, dodany za pomocą serwisu )(?:.*)\(https:\/\/(?:.*)\)/g, "");
     let c = b.replace(/(\n\*\*\*\n\n###\sOryginally posted on \[)(.*)(\)\.\sSteem blog powered by \[)(.*)(\)\.)/g, "");
-    let d = c.replace(/(\n\*\*\*\n<center>\s###\sPierwotnie opublikowano na \[)(.*)(\)\.\sBlog na Steem napędzany przez \[)(.*)(\)\.\n\<\/center\>)/g, "")
+    let d = c.replace(/(\n\*\*\*\n\s###\sPierwotnie opublikowano na \[)(.*)(\)\.\sBlog na Steem napędzany przez \[)(.*)(\)\.)/g, "");
     return d;
 }
 
