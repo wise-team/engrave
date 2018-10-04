4b83f4b6d95aeaad00e9009d204b7eb9e45a33fa
Bartłomiej Górnicki <brutusgsm@gmail.com>
2018-10-05 00:57:02 +0200

    Nodeapps fix

--- src/modules/NodeApps.ts
+++ src/modules/NodeApps.ts
@@ -12,45 +12,51 @@ export class NodeAppsModule {
 
     static async createAndRun(domain: string, port: any, steem_username: string) {
 
-        await pm2.connect();
-
-        ncp(path.join(__dirname, '../../blog_app_template'), path.join(__dirname, '../../pm2_blogs/' + domain), function (err: Error) {
-            if (err) {
-              console.log("NCP error: ", err);
+        pm2.connect((err: Error) => {
+            if(err) {
+                console.log(err);
             } else {
-                let newAppConfig = {
-                    apps: [{
-                        name: domain,
-                        script: './pm2_blogs/' + domain + '/app.js',
-                        watch: false,
-                        env: {
-                            PORT: port,
-                            STEEM_USERNAME: steem_username,
-                            DATABASE_URL: config.database_url,
-                            NODE_ENV: "development"
-                        },
-                    }]
-                };
-        
-                fs.writeFile(path.join(__dirname, '../../pm2_blogs/' + domain + '/app_config.json'), JSON.stringify(newAppConfig), { flag: 'w' } , (err: Error) => {
-                    if (!err) {
-                        console.log(' * Copying files completed.');
-        
-                        pm2.start(path.join(__dirname, '../../pm2_blogs/' + domain + '/app_config.json'), function (err: Error, apps: any) {
-        
-                            if (err) {
-                                console.log(err);
-                            }
-        
-                            pm2.disconnect();   // Disconnects from PM2
-                            console.log(' * New blog with domain: ' + domain + ' for: @' + steem_username + " is ready!");
-                        });
-        
+                ncp(path.join(__dirname, '../../blog_app_template'), path.join(__dirname, '../../pm2_blogs/' + domain), function (err: Error) {
+                    if (err) {
+                      console.log("NCP error: ", err);
                     } else {
-                        console.log(" * Copying files error:", err);
+                        console.log("NCP copied");
+                        let newAppConfig = {
+                            apps: [{
+                                name: domain,
+                                script: './pm2_blogs/' + domain + '/app.js',
+                                watch: false,
+                                env: {
+                                    PORT: port,
+                                    STEEM_USERNAME: steem_username,
+                                    DATABASE_URL: config.database_url,
+                                    NODE_ENV: "development"
+                                },
+                            }]
+                        };
+                
+                        fs.writeFile(path.join(__dirname, '../../pm2_blogs/' + domain + '/app_config.json'), JSON.stringify(newAppConfig), { flag: 'w' } , (err: Error) => {
+                            if (!err) {
+                                console.log(' * Copying files completed.');
+                
+                                pm2.start(path.join(__dirname, '../../pm2_blogs/' + domain + '/app_config.json'), function (err: Error, apps: any) {
+                
+                                    if (err) {
+                                        console.log(err);
+                                    }
+                
+                                    pm2.disconnect();   // Disconnects from PM2
+                                    console.log(' * New blog with domain: ' + domain + ' for: @' + steem_username + " is ready!");
+                                });
+                
+                            } else {
+                                console.log(" * Copying files error:", err);
+                            }
+                        });
                     }
                 });
             }
         });
+        
     };
 }
