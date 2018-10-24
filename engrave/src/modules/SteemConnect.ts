385095dfb3ad95f1117f326364eec6077b21bd9a
Bartłomiej Górnicki <bartlomiej@gornicki.org>
2018-10-24 03:04:43 +0200

    Fix #1 Steemconnect requests too much permissions when login on user's blog

--- engrave/src/modules/SteemConnect.ts
+++ engrave/src/modules/SteemConnect.ts
@@ -1,10 +1,17 @@
 import { Config, IConfig } from "../config";
+
 const sc2 = require('steemconnect');
 
 const config: IConfig = Config.GetConfig();
 
-export const SteemConnect = sc2.Initialize({
+export const AdminSteemConnect = sc2.Initialize({
     app: config.steemconnect_id,
     callbackURL: config.steemconnect_redirect_uri,
     scope: ['login', 'vote', 'comment', 'comment_options', 'claim_reward_balance','delete_comment']
+});
+
+export const UserSteemConnect = sc2.Initialize({
+    app: config.steemconnect_id,
+    callbackURL: config.steemconnect_redirect_uri,
+    scope: ['login', 'vote', 'comment']
 });
