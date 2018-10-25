a39be34756b675096e8be5b6c967e067eb715b65
Bartłomiej Górnicki <bartlomiej.gornicki@gmail.com>
2018-10-25 02:23:36 +0200

    Resolve #8 Check for certificates when recreating nginx configuration (#9)

--- engrave/src/modules/Nginx.ts
+++ engrave/src/modules/Nginx.ts
@@ -8,6 +8,9 @@ let config = Config.GetConfig();
 
 export class NginxModule {
 
+    private static nginxConfDirectory = '/etc/nginx/conf.d/';
+    private static sslCertificatesDirectory = '/etc/letsencrypt/live';
+
     private static subdomain_template = 'server {\r\n\tserver_name SUBDOMAIN;\r\n\treturn 301 https://SUBDOMAIN$request_uri;\r\n}\r\n\r\nserver {\r\n\tlisten 443 ssl;\r\n\tserver_name SUBDOMAIN;\r\n\r\n\tssl_certificate \/etc\/letsencrypt\/live\/DOMAIN\/fullchain.pem;\r\n\tssl_certificate_key \/etc\/letsencrypt\/live\/DOMAIN\/privkey.pem;\r\n\t\r\nlocation \/ {\r\n\tproxy_pass http:\/\/engrave:PORT;\r\n\tproxy_http_version 1.1;\r\n\tproxy_set_header Upgrade $http_upgrade;\r\n\tproxy_set_header Connection \'upgrade\';\r\n\tproxy_set_header Host $host;\r\n\tproxy_cache_bypass $http_upgrade;\r\n\t}\r\n}';
     private static domain_template = 'server {\n\tlisten 80;\n\n\tserver_name EXAMPLE www.EXAMPLE;\n\nlocation \/ {\n\tproxy_pass http:\/\/engrave:PORT;\n\tproxy_http_version 1.1;\n\tproxy_set_header Upgrade $http_upgrade;\n\tproxy_set_header Connection \'upgrade\';\n\tproxy_set_header Host $host;\n\tproxy_cache_bypass $http_upgrade;\n   }\n}\n\n';
     private static ssl_domain_template = 'server {\n\tserver_name www.EXAMPLE EXAMPLE;\n\treturn 301 https://EXAMPLE$request_uri;\n}\n\nserver {\n\tlisten 443 ssl;\n\tserver_name www.EXAMPLE;\n\n\tssl_certificate /etc/letsencrypt/live/EXAMPLE/fullchain.pem;\n\tssl_certificate_key /etc/letsencrypt/live/EXAMPLE/privkey.pem;\n\t\n\treturn 301 https://EXAMPLE$request_uri;\n}\n\nserver {\n\tlisten 443 ssl;\n\tserver_name EXAMPLE;\n\n\tssl_certificate /etc/letsencrypt/live/EXAMPLE/fullchain.pem;\n\tssl_certificate_key /etc/letsencrypt/live/EXAMPLE/privkey.pem;\n\n\tlocation / {\n\tproxy_pass http://engrave:PORT;\n\tproxy_http_version 1.1;\n\tproxy_set_header Upgrade $http_upgrade;\n\tproxy_set_header Connection \'upgrade\';\n\tproxy_set_header Host $host;\n\tproxy_cache_bypass $http_upgrade;\n\t}\n}\n';
@@ -15,7 +18,7 @@ export class NginxModule {
     static async generateNginxSettings(blog: IBlog) {
         try {
             let configFilename = blog.port.toString() + '_' + blog.domain+ '.conf';
-            let configFilePath = path.join('/etc/nginx/conf.d/', configFilename);
+            let configFilePath = path.join(this.nginxConfDirectory, configFilename);
             let configContent = this.generateConfigFileContent(blog);
             fs.writeFileSync(configFilePath, configContent);
         } catch (error) {
@@ -25,10 +28,10 @@ export class NginxModule {
     }
 
     static generateCustomDomainConfigWithSSL(domain: string, port: any, cb: any) {
-        let tmp1 = this.ssl_domain_template.replace(/EXAMPLE/g, domain);
+        let tmp1 = this.ssl_domain_template.replace(/E=XAMPLE/g, domain);
         let tmp2 = tmp1.replace(/PORT/g, port);
 
-        fs.writeFile("/etc/nginx/conf.d/" + port + '_' + domain + ".conf", tmp2, (err: Error) => {
+        fs.writeFile(this.nginxConfDirectory + port + '_' + domain + ".conf", tmp2, (err: Error) => {
             if (!err) {
                 console.log(' * Domain config for NGINX has been saved successfully');
             }
@@ -39,7 +42,7 @@ export class NginxModule {
         });
     }
 
-    static isBlogDomainCorrect(domain: string) {
+    static isPortalDomainCorrect(domain: string) {
         for (let i in config.blogs_domains) {
             if (config.blogs_domains[i] == domain) {
                 return true;
@@ -48,7 +51,7 @@ export class NginxModule {
         return false;
     }
 
-    static getDomainFromString(domain: string) {
+    static getDomainFromSubdomainString(domain: string) {
         if (domain) {
             let parts = domain.split('.');
             if (parts.length >= 2) {
@@ -66,14 +69,33 @@ export class NginxModule {
     }
 
     private static generateConfigForSubdomain(blog: IBlog) {
-        let tmp1 = this.subdomain_template.replace(/SUBDOMAIN/g, blog.domain);
-        let tmp2 = tmp1.replace(/DOMAIN/g, NginxModule.getDomainFromString(blog.domain));
-        return tmp2.replace(/PORT/g, blog.port.toString());
+        if (this.validateDomainCertificates(this.getDomainFromSubdomainString(blog.domain))){
+            let tmp1 = this.subdomain_template.replace(/SUBDOMAIN/g, blog.domain);
+            let tmp2 = tmp1.replace(/DOMAIN/g, NginxModule.getDomainFromSubdomainString(blog.domain));
+            return tmp2.replace(/PORT/g, blog.port.toString());
+        } else {
+            let tmp1 = this.domain_template.replace(/EXAMPLE/g, blog.domain);
+            return tmp1.replace(/PORT/g, blog.port.toString());
+        }
+        
     }
 
     private static generateConfigForCustomDomain(blog: IBlog) {
-        let tmp1 = this.domain_template.replace(/EXAMPLE/g, blog.domain);
+        let template = this.domain_template;
+        if(this.validateDomainCertificates(blog.domain)) {
+            template = this.ssl_domain_template;
+        }
+        let tmp1 = template.replace(/EXAMPLE/g, blog.domain);
         return tmp1.replace(/PORT/g, blog.port.toString());
+    }
+
+    private static validateDomainCertificates(domain: string) {
+        const certificatesPath = path.join(this.sslCertificatesDirectory, domain);
+        const fullchainPath = path.join(certificatesPath, 'fullchain.pem');
+        const privkeyPath = path.join(certificatesPath, 'privkey.pem');
+
+        if (fs.existsSync(fullchainPath) && fs.existsSync(privkeyPath)) return true;
+        else return false;
     }
 
 }
---
+++ engrave/test/modules/Nginx.spec.ts
@@ -0,0 +1,29 @@
+import { NginxModule } from './../../src/modules/Nginx';
+import { describe } from 'mocha';
+import { expect } from 'chai';
+import { TestUtils } from '../TestUtils';
+
+describe('Nginx', function () {
+
+    describe('getDomainFromString', function () {
+        this.timeout(5000);
+
+        beforeEach(async () => {
+            await TestUtils.prepare();
+        })
+
+        it('Should return domain from subdomain string', () => {
+            expect(NginxModule.getDomainFromSubdomainString('sub.engrave.website')).equal('engrave.website');
+        });
+
+        it('Should return dmain from top level domain string', () => {
+            expect(NginxModule.getDomainFromSubdomainString('example.com')).equal('example.com');
+        });
+
+        it('Should return null from invalid string', () => {
+            expect(NginxModule.getDomainFromSubdomainString('invalid')).to.be.null;
+        });
+
+    });
+
+});
