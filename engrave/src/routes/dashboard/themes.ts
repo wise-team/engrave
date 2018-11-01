51d0cc96867f42d99b7314c021091603fb892958
Bartłomiej Górnicki <bartlomiej@gornicki.org>
2018-11-01 17:33:34 +0100

    WIP Template page

--- engrave/src/routes/dashboard/themes.ts
+++ engrave/src/routes/dashboard/themes.ts
@@ -1,11 +1,16 @@
 import * as express from 'express';
 import { IExtendedRequest } from '../IExtendedRequest';
 import { GetValidators } from '../../validators/GetValidators';
+import { Themes } from '../../modules/Themes';
 
 let router = express.Router();
 
 router.get('/themes', GetValidators.isLoggedAndConfigured, (req: IExtendedRequest, res: express.Response) => {
-    res.render('dashboard/themes.pug', { blogger: req.session.blogger, url: 'themes' });
+    res.render("dashboard/themes.pug", {
+      themes: Themes.getInstalledThemes(),
+      blogger: req.session.blogger,
+      url: "themes"
+    });
 });
 
 module.exports = router;
