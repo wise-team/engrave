44de1dc248f1130d0d1b69a0f2f6ad8938331875
Bartłomiej Górnicki <bartlomiej@gornicki.org>
2018-11-01 20:20:55 +0100

    Themes page for theme selecting (custom themes not available yet)

---
+++ engrave/src/validators/ThemeValidator.ts
@@ -0,0 +1,17 @@
+import { Themes } from './../modules/Themes';
+import { IExtendedRequest } from "../helpers/IExtendedRequest";
+import * as express from 'express';
+import { ValidatorUtils } from "./ValidatorUtils";
+
+export class ThemeValidator {
+    static async ValidateInput(req: IExtendedRequest, res: express.Response, next: express.NextFunction) {
+        try {
+            if (!ValidatorUtils.isExist(req.body.theme)) throw new Error("Theme not provided");
+            if (!Themes.verifyTheme(req.body.theme)) throw new Error("Invalid theme");
+            
+            next();
+        } catch (error) {
+            res.status(400).json(error.message);
+        }
+    }
+}
