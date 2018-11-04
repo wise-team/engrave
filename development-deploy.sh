5548462c0dd0890cf3243d7da11f41699cbc3836
Bartłomiej Górnicki <bartlomiej@gornicki.org>
2018-11-04 22:00:27 +0100

    Minimize down time when updating containers

--- development-deploy.sh
+++ development-deploy.sh
@@ -1,6 +1,6 @@
-docker stack rm engrave
 
 docker build ./nginx/ -f ./nginx/Dockerfile.development -t bgornicki/nginx
 docker build ./engrave/ -f ./engrave/Dockerfile.development -t bgornicki/engrave
 
+docker stack rm engrave
 docker stack deploy -c docker-compose.development.yml engrave
--- production-deploy.sh
+++ production-deploy.sh
@@ -1,6 +1,6 @@
-docker stack rm engrave
 
 docker build ./nginx/ -f ./nginx/Dockerfile.production -t bgornicki/nginx
 docker build ./engrave/ -f ./engrave/Dockerfile.production -t bgornicki/engrave
 
+docker stack rm engrave
 docker stack deploy -c docker-compose.production.yml engrave
