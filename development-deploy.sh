605cce24d18da9e1345fd302f3f8952d650f25b6
Bartłomiej Górnicki <brutusgsm@gmail.com>
2018-11-19 14:16:54 +0100

    Deployment scripts fixed to wait for docker network being created

--- development-deploy.sh
+++ development-deploy.sh
@@ -3,4 +3,15 @@ docker build ./nginx/ -f ./nginx/Dockerfile.development -t bgornicki/nginx
 docker build ./engrave/ -f ./engrave/Dockerfile.development -t bgornicki/engrave
 
 docker stack rm engrave
+
+until [ -z "$(docker service ls --filter label=com.docker.stack.namespace=engrave -q)" ] || [ 60 -lt 0 ]; do
+  echo 'Waiting for services to be removed...'
+  sleep 1;
+done
+
+until [ -z "$(docker network ls --filter label=com.docker.stack.namespace=engrave -q)" ] || [ 60 -lt 0 ]; do
+  echo 'Waiting for network to be removed...'
+  sleep 1;
+done
+
 docker stack deploy -c docker-compose.development.yml engrave
