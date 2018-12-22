d6f5cac7a14f4f2e1908640baff55af89c6f8351
Bartlomiej Gornicki <brutusgsm@gmail.com>
2018-12-23 00:25:27 +0100

    Show latest articles

--- engrave/src/routes/frontpage/explore.ts
+++ engrave/src/routes/frontpage/explore.ts
@@ -3,16 +3,19 @@ import { Response, NextFunction } from "express";
 import * as express from "express";
 import { validationResult, check } from 'express-validator/check';
 import { IExtendedRequest } from '../../helpers/IExtendedRequest';
+import { PublishedArticlesModule } from '../../modules/PublishedArticles';
 
 let router = express.Router();
 
 router.get("/explore", renderExplorePage);
-router.post("/explore", check("skip").isDecimal(), getMoreBlogs);
+router.post("/explore/blogs", check("skip").isDecimal(), getMoreBlogs);
+router.post("/explore/articles", check("skip").isDecimal(), getMoreArticles);
 
 
 async function renderExplorePage(req: IExtendedRequest, res: Response, next: NextFunction) {
   res.render("main/explore.pug", {
     blogs: await BlogListModule.getRegisteredBlogs(0),
+    articles: await PublishedArticlesModule.getLatest(0),
     blogger: req.session.blogger
   });
 }
@@ -27,6 +30,19 @@ async function getMoreBlogs(req: IExtendedRequest, res: Response, next: NextFunc
 
   res.json({
     blogs: await BlogListModule.getRegisteredBlogs(parseInt(req.body.skip))
+  });
+}
+
+async function getMoreArticles(req: IExtendedRequest, res: Response, next: NextFunction) {
+
+  const errors = validationResult(req);
+  
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  res.json({
+    articles: await PublishedArticlesModule.getLatest(parseInt(req.body.skip))
   });
 }
 
