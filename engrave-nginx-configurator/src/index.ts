cfcad510f5f20d259126e1015b24eb32bd6c7736
Bartłomiej Górnicki <brutusgsm@gmail.com>
2019-01-09 19:58:11 +0100

    Nginx configurator should generate config on startup and only for custom domains

--- engrave-nginx-configurator/src/index.ts
+++ engrave-nginx-configurator/src/index.ts
@@ -1,4 +1,26 @@
 import app from './app/app';
 import { listenOnPort } from './submodules/engrave-shared/utils/listenOnPort';
+import waitForMicroservice from './submodules/engrave-shared/utils/waitForMicroservice';
+import config from './submodules/engrave-shared/config/config';
+import { Blogs } from './submodules/engrave-shared/models/BlogsModel';
+import generateNginxSettings from './services/nginx/generateSettings';
 
-listenOnPort(app, 3000);
+( async() => {
+    
+    await waitForMicroservice(config.services.ssl);
+    
+    try {
+        const blogs = await Blogs.find({configured: true, is_domain_custom: true});
+        
+        for(const blog of blogs) {
+            await generateNginxSettings(blog.domain, blog.port, blog.is_domain_custom);
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+    listenOnPort(app, 3000);
+
+} )();
+
