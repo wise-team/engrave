da87075d7f5e9339ff706d70cf2504fd7674b341
Bartłomiej Górnicki <brutusgsm@gmail.com>
2019-01-09 19:08:31 +0100

    Upgraded nginx configuration, dockerfiles and deployment scripts

--- production-deploy.engrave.sh
+++ production-deploy.engrave.sh
@@ -1,22 +1,11 @@
-
-docker build ./nginx/ -f ./nginx/Dockerfile.engrave.production -t bgornicki/nginx
-docker build ./engrave/ -f ./engrave/Dockerfile.production -t bgornicki/engrave
-docker build ./engrave-statistics/ -f ./engrave-statistics/Dockerfile.production -t bgornicki/engrave-statistics
-docker build ./engrave-image-uploader/ -f ./engrave-image-uploader/Dockerfile.production -t bgornicki/engrave-image-uploader
 docker build ./engrave-api-gateway/ -f ./engrave-api-gateway/Dockerfile.production -t bgornicki/engrave-api-gateway
-docker build ./engrave-ssl/ -f ./engrave-ssl/Dockerfile.production -t bgornicki/engrave-ssl
+docker build ./engrave-blockchain-tracker/ -f ./engrave-blockchain-tracker/Dockerfile.production -t bgornicki/engrave-blockchain-tracker
+docker build ./engrave-blogs-renderer/ -f ./engrave-blogs-renderer/Dockerfile.production -t bgornicki/engrave-blogs-renderer
+docker build ./engrave-front-dashboard/ -f ./engrave-front-dashboard/Dockerfile.production -t bgornicki/engrave-front-dashboard
+docker build ./engrave-image-uploader/ -f ./engrave-image-uploader/Dockerfile.production -t bgornicki/engrave-image-uploader
 docker build ./engrave-nginx-configurator/ -f ./engrave-nginx-configurator/Dockerfile.production -t bgornicki/engrave-nginx-configurator
-
-docker stack rm engrave
-
-until [ -z "$(docker service ls --filter label=com.docker.stack.namespace=engrave -q)" ] || [ 60 -lt 0 ]; do
-  echo 'Waiting for services to be removed...'
-  sleep 1;
-done
-
-until [ -z "$(docker network ls --filter label=com.docker.stack.namespace=engrave -q)" ] || [ 60 -lt 0 ]; do
-  echo 'Waiting for network to be removed...'
-  sleep 1;
-done
+docker build ./engrave-ssl/ -f ./engrave-ssl/Dockerfile.production -t bgornicki/engrave-ssl
+docker build ./engrave-statistics/ -f ./engrave-statistics/Dockerfile.production -t bgornicki/engrave-statistics
+docker build ./nginx/ -f ./nginx/Dockerfile.dblog.production -t bgornicki/nginx
 
 docker stack deploy -c docker-compose.engrave.production.yml engrave
