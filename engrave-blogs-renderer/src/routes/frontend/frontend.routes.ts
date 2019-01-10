fd9b1658ad5b4d660a6f3ac900f5a47234abe5d5
Bartłomiej Górnicki <brutusgsm@gmail.com>
2019-01-10 14:42:49 +0100

    More things in template repaired. Adding articles to category queue

--- engrave-blogs-renderer/src/routes/frontend/frontend.routes.ts
+++ engrave-blogs-renderer/src/routes/frontend/frontend.routes.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import main from "./routes/main";
 import article from "./routes/article";
+import category from "./routes/category";
 
 const frontendApi: express.Router = express.Router();
 
 frontendApi.get('/', main.middleware, main.handler);
+frontendApi.get('/category/:slug', category.middleware, category.handler);
 frontendApi.get('/:permlink', article.middleware, article.handler);
 
 export default frontendApi;
---
+++ engrave-blogs-renderer/src/routes/frontend/routes/category.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import { getBlog, getLatestFromCategory, getFeaturedArticles } from '../../../submodules/engrave-shared/services/cache/cache';
+import { BlogNotExist } from '../../../submodules/engrave-shared/helpers/errorCodes';
+
+const middleware: any[] =  [];
+
+async function handler(req: Request, res: Response) {
+    
+    const {hostname} = req;
+    const {slug} = req.params;
+    
+    try {
+        
+        const blog = await getBlog(hostname);
+        const latest = await getLatestFromCategory(slug, blog.username, 10);
+        const featured = await getFeaturedArticles(blog.username, 10);
+
+        // dynamicStatic.setPath(path.resolve(__dirname, 'path/to/app/assets'));
+        
+        return res.render('default/theme/category.pug', {
+            blog: blog,
+            latest: latest,
+            featured: featured
+        });
+        
+    } catch(error) {
+        if(error instanceof BlogNotExist) {
+            return res.redirect('https://' + process.env.DOMAIN);
+        } else {
+            return res.redirect('/');
+        }
+    }
+}
+
+export default {
+    middleware,
+    handler
+}
