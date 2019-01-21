0f2cca658e9c6df1281202b8d8a57c871d23393d
Bartłomiej Górnicki <brutusgsm@gmail.com>
2019-01-21 22:59:37 +0100

    Visual changes

--- engrave-api-gateway/src/middlewares/jwt/verifyToken.ts
+++ engrave-api-gateway/src/middlewares/jwt/verifyToken.ts
@@ -9,19 +9,23 @@ class VerificationError extends Error {
 export default async (req: Request, res: Response, next: NextFunction) => {
     
     try {
-        if(!req.headers['authorization']) throw new VerificationError('Auth token was not supplied');
+        if(!req.headers['authorization']) 
+            throw new VerificationError('Auth token was not supplied');
         
         const token = (<string>req.headers['authorization']).replace("Bearer ", "");
 
-        if(!token) throw new VerificationError('Unauthorized');
+        if( ! token) 
+            throw new VerificationError('Unauthorized');
     
         const decodedToken: any = jwt.decode(token);
 
-        if(decodedToken.proxy !== "engrave.app") throw new VerificationError('Wrong proxy name');
+        if(decodedToken.proxy !== "engrave.app") 
+            throw new VerificationError('Wrong proxy name');
 
         const currentTime = new Date().getTime() / 1000;
         
-        if (decodedToken.exp < currentTime) throw new VerificationError("Token expired");
+        if (decodedToken.exp < currentTime) 
+            throw new VerificationError("Token expired");
 
         next();
         
