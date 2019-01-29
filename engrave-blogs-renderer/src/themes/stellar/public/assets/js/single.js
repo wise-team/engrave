8e2e3a195aa79a9f8f6fcb82d769e175b282a85e
Bartłomiej Górnicki <brutusgsm@gmail.com>
2019-01-29 19:08:06 +0100

    Comments and article infos on Stellar theme

--- engrave-blogs-renderer/src/themes/stellar/public/assets/js/single.js
+++ engrave-blogs-renderer/src/themes/stellar/public/assets/js/single.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
     getAndRenderComments(editorial, permlink, "#comments");
 
 });
+
 function getLoggedInToken(){
     return localStorage.getItem('aMZr1grXqFXbiRzmOGRM');
 }
@@ -15,7 +16,7 @@ function getAndRenderComments(username, permlink, target_element) {
     steem.api.getContentReplies(username, permlink, function (err, result) {
         if (!err && result) {
             if(result.length) {
-                // $('#comments-empty').remove();
+                $('#comments-empty').remove();
             }
             (function(ul) {
                 renderCommentsList(result, ul);
@@ -27,7 +28,7 @@ function getAndRenderComments(username, permlink, target_element) {
 function renderCommentsList(comments, list_id) {
     
     let ul = document.createElement('ul');
-    $(ul).addClass("comments");
+    $(ul).addClass("comments").addClass('blog-level');
     $(list_id).append(ul);
 
     comments.forEach(comment => {
@@ -50,14 +51,13 @@ function renderCommentsList(comments, list_id) {
             }
         })(comment, ul);
 
-  
     });
 }
 
 function renderComment(comment, voted, list_id) {
     
-    const newCommentHtml = `<img src="https://steemitimages.com/u/${comment.author}/avatar" class="avatar"><div class="post-comments"><p class="meta">${comment.published}, <input type="hidden" name="comment_permlink" value="${comment.permlink}"><input type="hidden" name="comment_author" value="${comment.author}"><a href="https://steemit.com/@${comment.author}">${comment.author}</a>: <i class="pull-right">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-thumbs-up comments-action comment-vote"></i>&nbsp;<span name="comment-votes">${comment.net_votes}</span>&nbsp;&nbsp;&nbsp;&nbsp;<span name="comment-value">$0.00</span><span class="comment-action comment-reply">&nbsp;&nbsp;&nbsp;&nbsp;Reply</span></i></p>${comment.body}<p>
-    asdasd</p></div>`;
+    const value = parseFloat(parseFloat(comment.pending_payout_value.replace(" SBD", "")) + parseFloat(comment.total_payout_value.replace(" SBD", ""))).toFixed(2);
+    const newCommentHtml = `<img src="https://steemitimages.com/u/${comment.author}/avatar" class="avatar"><div class="post-comments"><p class="meta"><a href="https://steemit.com/@${comment.author}">@${comment.author}</a>, <span title="${moment.utc(comment.created).local().format('LLL')}">${moment.utc(comment.created).local().fromNow()}</span>: <input type="hidden" name="comment_permlink" value="${comment.permlink}"><input type="hidden" name="comment_author" value="${comment.author}"> </p><p>${marked(comment.body)}</p><p class="comment-actions"><i class="fa fa-thumbs-up comments-action comment-vote"></i>&nbsp;<span name="comment-votes">${comment.net_votes}</span>&nbsp;&nbsp;&nbsp;&nbsp;<span name="comment-value">$${value}</span><span class="comment-action comment-reply">&nbsp;&nbsp;&nbsp;&nbsp;Reply</span></p></div>`;
 
     $(list_id).append(newCommentHtml); 
 
