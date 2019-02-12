ff787ef519d2faec7a10c0be0c63fd0fad83ebd6
Bartłomiej Górnicki <brutusgsm@gmail.com>
2019-02-12 13:32:58 +0100

    Cors added to API gateway

--- engrave-api-gateway/src/app/app.settings.ts
+++ engrave-api-gateway/src/app/app.settings.ts
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 function settings(app: any) {
     mongoose.connect("mongodb://mongo:27017/engrave", { useNewUrlParser: true });
+    app.use(cors);
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
 }
