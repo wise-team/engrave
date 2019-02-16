cfb8980ebbad4a88dc8da96edcccf4143af3237f
Bartłomiej Górnicki <brutusgsm@gmail.com>
2019-02-16 14:09:03 +0100

    Develop local theme in easy way

--- engrave-api-gateway/src/app/app.settings.ts
+++ engrave-api-gateway/src/app/app.settings.ts
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 function settings(app: any) {
     mongoose.connect("mongodb://mongo:27017/engrave", { useNewUrlParser: true });
-    app.use(cors);
+    app.use(cors());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
 }
