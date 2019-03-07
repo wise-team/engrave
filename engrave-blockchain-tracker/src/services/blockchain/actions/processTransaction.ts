8ea2fb786a962f7e567dad699f45ea88a085d2fd
Bartłomiej Górnicki <brutusgsm@gmail.com>
2019-03-07 19:42:47 +0100

    Prevent blockchain tracker from freezing when consuming uncategorized article

--- engrave-blockchain-tracker/src/services/blockchain/actions/processTransaction.ts
+++ engrave-blockchain-tracker/src/services/blockchain/actions/processTransaction.ts
@@ -1,5 +1,6 @@
-import { ifArticleExist, deleteArticle, isUserRegistered } from "../../redis/redis";
+import { deleteArticle } from "../../redis/redis";
 import { IUpdate } from "../blockchain";
+import {ifArticleExist, isUserRegistered} from '../../../submodules/engrave-shared/services/cache/cache';
 
 export default async (tx: any): Promise<IUpdate> => {
     try {
@@ -28,13 +29,13 @@ export default async (tx: any): Promise<IUpdate> => {
                     if(mentions) {
                         for(const mention of mentions) {
                             if(await isUserRegistered(mention.replace('@', ''))) {
-                                console.log(" * Mention:", mention);
+                                console.log("Mention:", mention);
                             }
                         }
                     }
                     
                     if( parent_author != author && await isUserRegistered(parent_author)) {
-                        console.log(" * New comment: ", parent_author, author, permlink);
+                        console.log("New comment: ", parent_author, author, permlink);
                     }
 
                     if(await ifArticleExist(author, permlink)) {
@@ -59,7 +60,7 @@ export default async (tx: any): Promise<IUpdate> => {
                     const { from, to, amount } = operation[1];
                     
                     if(await isUserRegistered(to)) {
-                        console.log(" * New transfer: ", from, to, amount);
+                        console.log("New transfer: ", from, to, amount);
                     }
                 }
                 break;
@@ -78,9 +79,9 @@ export default async (tx: any): Promise<IUpdate> => {
 
                             if(await isUserRegistered(following)) {
                                 if( what.length > 0) {
-                                    console.log(" * Started following you: ", follower, following);
+                                    console.log("Started following you: ", follower, following);
                                 } else {
-                                    console.log(" * Stopped following you: ", follower, following);
+                                    console.log("Stopped following you: ", follower, following);
                                 }
                             }
                         }
