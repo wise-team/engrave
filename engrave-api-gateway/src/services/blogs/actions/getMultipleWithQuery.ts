eca3b2cf2dc1312949a77cdb1fdabd4565ebe67a
Bartłomiej Górnicki <bartlomiej@gornicki.org>
2019-03-18 20:45:31 +0100

    Categories should be returned only when ask for specific blog

--- engrave-api-gateway/src/services/blogs/actions/getMultipleWithQuery.ts
+++ engrave-api-gateway/src/services/blogs/actions/getMultipleWithQuery.ts
@@ -1,12 +1,7 @@
 import { Blogs } from "../../../submodules/engrave-shared/models/Blogs";
-import categoriesService from "../../categories/categories.service";
 
 async function getMultipleWithQuery(query: any) {
     let blogs = await Blogs.find(query);
-
-    for (let blog of blogs) {
-        blog.categories = await categoriesService.getCategoriesByBlogId(blog._id);
-    }
 
     return blogs
 }
