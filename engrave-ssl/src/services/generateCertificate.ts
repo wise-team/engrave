a4ffd2ad2bf947d335d59e588ae814e42a4417a7
Bartłomiej Górnicki <bartlomiej@gornicki.org>
2019-04-16 20:03:59 +0200

    Debug SSL certificate failure

--- engrave-ssl/src/services/generateCertificate.ts
+++ engrave-ssl/src/services/generateCertificate.ts
@@ -6,15 +6,17 @@ const leStore = require('le-store-certbot').create({
     chainPath: ':configDir/live/:hostname/chain.pem',              //       greenlock.js
     logsDir: process.env.SSL_CERTIFICATES_DIR + '/engrave-logs',
     webrootPath: '/app/certbot/.well-known/acme-challenge',
-    debug: false
+    debug: true
 });
+
+const acmeServer = process.env.NODE_ENV === 'production' ? "https://acme-v02.api.letsencrypt.org/directory" : "https://acme-staging-v02.api.letsencrypt.org/directory"
 
 export default async (domain: string) => {
     try {
         console.log(' * Trying to generate certificates');
 
         const opts = { domains: [domain, 'www.' + domain], email: process.env.SSL_EMAIL, agreeTos: true, communityMember: false };
-        const greenlock = require('greenlock').create({ version: 'draft-12', server: 'https://acme-v02.api.letsencrypt.org/directory', store: leStore });
+        const greenlock = require('greenlock').create({ version: 'draft-12', server: acmeServer, store: leStore });
 
         await greenlock.register(opts);
 
