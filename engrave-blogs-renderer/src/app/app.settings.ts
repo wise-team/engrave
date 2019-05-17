793057f27c3902310e386a87854af13e8a4bbc83
Bartłomiej Górnicki <bartlomiej.gornicki@gmail.com>
2019-05-17 23:08:27 +0200

    The key to do themes localization

--- engrave-blogs-renderer/src/app/app.settings.ts
+++ engrave-blogs-renderer/src/app/app.settings.ts
@@ -1,6 +1,8 @@
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const serveStatic = require('serve-static')
+const i18n = require('i18n-x');
+
 import * as express from 'express';
 import { getBlog } from '../submodules/engrave-shared/services/cache/cache';
 
@@ -8,19 +10,39 @@ function settings(app: any) {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use('/robots.txt', serveRobotsTxt);
-    app.use(serveStaticForTheme);
+    app.use(handleThemeStatics);
+    app.use(handleLocalization);
     app.use(express.static('/app/certbot')); // for letsencrypt purposes
     app.set('view engine', 'pug');
     app.set('views', '/app/src/themes');
     mongoose.connect("mongodb://mongo:27017/engrave", { useNewUrlParser: true });
 }
 
-const serveStaticForTheme = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+const handleThemeStatics = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const {hostname} = req;
         const blog = await getBlog(hostname);            
         const staticHandler = serveStatic(`/app/src/themes/${blog.theme}/public`)
         return staticHandler(req, res, next);    
+    } catch (error) {
+        return next();
+    }
+}
+
+const handleLocalization = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const {hostname} = req;
+        const blog = await getBlog(hostname);            
+
+        const localizationHandler = i18n({ 
+            locales: ['en', 'pl'],
+            baseDir: `/app/src/themes/${blog.theme}`,
+            directory: 'locales',
+            order: [],
+            locale: 'en'
+        });
+        return localizationHandler(req, res, next);    
+
     } catch (error) {
         return next();
     }
